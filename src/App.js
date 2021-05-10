a34b65e4931f77a19e6a267803c228a19e7c0c0d
vadimdoga <vadimca37@gmail.com>
2021-05-10 10:59:49 +0300

    initiated components dir, integrated in App.js routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { Switch, Route } from "react-router-dom"
+import SignIn from "./components/auth/SignIn"
+import Register from "./components/auth/Register"
+import Homepage from "./components/NotLogged/Homepage"
+import CourseDetails from "./components/NotLogged/CourseDetails"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path="/" component={Homepage} />
+                <Route exact path="/courses/:grade_id" component={CourseDetails} />
+                <Route exact path="/auth/login" component={SignIn} />
+                <Route exact path="/auth/register" component={Register} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
